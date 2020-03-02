49e257f30d781c530b5a42f2f080a330436ebded
Monika Maleszka <maleszka.m93@gmail.com>
2020-03-02 12:36:34 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gradientBtn
+
---
+++ index.js
@@ -0,0 +1,59 @@
+
+
+function gradientBtn(options){
+  let buttons = document.querySelectorAll('.gradient-btn');
+
+  switch(options.color){
+    case 'red':
+      options.gradient = ' #EF3B36 0%, #FFFFFF 51%, #EF3B36 100%';
+      break;
+    case 'green':
+      options.gradient = '#1D976C 0%, #93F9B9 51%, #1D976C 100%';
+      break;
+    case 'blue':
+      options.gradient = '#1FA2FF 0%, #12D8FA 51%, #1FA2FF 100%';
+      break;
+    case 'yellow':
+      options.gradient = '#FF8008 0%, #FFC837 51%, #FF8008 100%';
+      break;
+    case 'grey':
+      options.gradient = '#757F9A 0%, #D7DDE8 51%, #757F9A 100%';
+      break;
+    default:
+      options.gradient = '#757F9A 0%, #D7DDE8 51%, #757F9A 100%';
+      break;
+  }
+
+    
+   
+
+  buttons.forEach(btn => {
+
+    btn.style.backgroundImage = `linear-gradient(to right, ${options.gradient} )`;
+    btn.style.backgroundSize = '200%';
+    btn.style.backgroundRepeat = 'no-repeat';
+    btn.style.transitionDuration = '0.5s'
+
+    btn.addEventListener('mouseenter', () => {
+      btn.style.backgroundPosition = 'right center';
+    })
+    btn.addEventListener('mouseleave', () => {
+      btn.style.backgroundPosition = 'left center';
+    })
+  })
+}
+
+
+
+module.exports.gradientBtn = gradientBtn;
+
+
+
+
+
+
+
+
+
+
+
